bbe1d10302805315052be6d7bf33570f4fe1e27d
IanSu18 <144109031+IanSu18@users.noreply.github.com>
2024-09-25 05:59:54 +0000

    create convergent

---
+++ Ian_Sprint_Projject_v1.6/Ian_Sprint_Project_v1.6.py
@@ -0,0 +1,13 @@
+def print_menu():
+    print()
+    print()
+def km_miles():
+def miles_km():
+
+if __name__ == '__main__':
+    print_menu()
+    choice = input('Which would you like to do today?: ')
+    if choice == '1':
+        km_mile()
+    if choice == '2':
+        miles_km()
